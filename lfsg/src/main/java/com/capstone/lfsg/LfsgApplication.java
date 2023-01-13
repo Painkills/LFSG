bf5292eae02b4aa44914bc16bccea0c845e7f2ac
David Fortich <d4tich@gmail.com>
2023-01-13 17:37:16 -0500

    Added base project from spring initializr

---
+++ lfsg/src/main/java/com/capstone/lfsg/LfsgApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.lfsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfsgApplication.class, args);
+	}
+
+}
---
+++ lfsg/src/test/java/com/capstone/lfsg/LfsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.lfsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
