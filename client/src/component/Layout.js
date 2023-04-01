e5f59aaeed2377dbe8d37b5d8be42d75e77a373f
David Fortich <d4tich@gmail.com>
2023-04-01 14:57:34 -0400

    Refactor of frontend. Got MainRoom working.

--- client/src/component/Layout.js
+++ client/src/component/Layout.js
@@ -8,9 +8,6 @@ const Layout = () => {
                     <li>
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
-                        <Link to="/RaidRoom">Raid Room</Link>
-                    </li>
                 </ul>
             </nav>
 
