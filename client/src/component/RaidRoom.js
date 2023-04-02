474eb8f86fd660b01b2066d7335fa041b28ee00f
David Fortich <d4tich@gmail.com>
2023-04-02 18:46:41 -0400

    reverted dockerize changes

--- client/src/component/RaidRoom.js
+++ client/src/component/RaidRoom.js
@@ -35,8 +35,13 @@ const RaidRoom = () => {
     }, []);
 
     const connect =()=>{
-        let Sock = new SockJS('https://lfsg1-d4tich.b4a.run/');
-        // let Sock = new SockJS('http://localhost:8082/ws');
+        // const Sock = new SockJS('https://lfsg1-d4tich.b4a.run/wss', null, {
+        //     transports: ['websocket'],
+        //     secure: true,
+        //     rejectUnauthorized: false
+        // });
+        // let Sock = new SockJS('https://lfsg1-d4tich.b4a.run/');
+        let Sock = new SockJS('http://localhost:8082/ws');
         stompClient = over(Sock);
         stompClient.connect({},onConnected, onError);
     }
